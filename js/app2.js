f81a1c45037c8a3eccfde6c2c5c7b2f68929ab53
Humberto Pineda <bpineda7540@gmail.com>
2022-07-17 21:31:43 -0700

    making sure question3 was answered

---
+++ js/app2.js
@@ -0,0 +1,22 @@
+
+
+// let adder = function(arr) {
+//   let sum = arr[0] + arr[1] +arr[2];
+//   let message = 'our sum is ${sum}';
+//   return [message, sum]
+// };
+
+// function adding() {
+//function code
+// }
+
+// let result = adder(numbers)[1];
+// console.log(result);
+
+// let result2 = adder(ages)[1];
+// console.log(result2);
+
+// let total = result + result2;
+// console.log(total);
+
+
