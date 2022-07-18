ed226806b11e6a29c560468cad47a8f0d01ea4da
Humberto Pineda <bpineda7540@gmail.com>
2022-07-17 20:43:37 -0700

    question 3 answered

--- js/app.js
+++ js/app.js
@@ -53,12 +53,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let sum1 = sum(a, b)[0];
+  let sum2 = sum(sum1, c)[0];
+  let product1 = multiply(a, b)[0];
+  let product2 = multiply(product1, c)[0];
+  let message1 = `${a} and ${b} and ${c} sum to ${sum2}.`;
+  let message2 = `The product of ${a} and ${b} and ${c} is ${product2}.`;
+  let arr = [sum2, product2, message1, message2];
+  return arr;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -76,7 +84,7 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let sum = 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
