45d20ed97c6156d6b3d48939caaa27512d43c290
Humberto Pineda <bpineda7540@gmail.com>
2022-07-17 23:09:32 -0700

    tried to solve question 4

--- js/app.js
+++ js/app.js
@@ -83,7 +83,7 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-  let sum = 
+
 }
 
 // Here is the test for sumArray(); uncomment it to run it
